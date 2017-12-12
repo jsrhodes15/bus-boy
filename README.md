ef68b074523e1655054eaf745563e6a29e809e2b
Jordan Rhodes <jorrhodes@deloitte.com>
2017-12-12 14:17:40 -0500

    Add eslint, export main module

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-*This package is not ready for prime time, hence the 0.0.1 version number. Stay tuned!*
+*This package is not ready for prime time, hence the <1.0.0 version number. Stay tuned!*
 
 ## Bus-Boy
 A collection of scripts used to interact with Azure Service Bus
@@ -27,3 +27,6 @@ npm -i bus-boy --save
    `peekSubscription`     - Peeklocks and displays first retrieved message in the specified subscription.\
    `subscriptionMsgCount` - Displays the current count of messages in the specified subscription.\
    `watchTopic`           - Creates a temporary subscription on the configured topic and displays incoming messages.\
+
+### Documentation for the _Azure SDK for Node_ can be found here:
+http://azure.github.io/azure-sdk-for-node/
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-import
+const createMessage = require('./scripts/createMessage.js');
+const createSubscription = require('./scripts/createSubscription.js');
+const createTopic = require('./scripts/createTopic.js');
+const deleteSubscription = require('./scripts/deleteSubscription.js');
+const deleteTopic = require('./scripts/deleteTopic.js');
+const listRules = require('./scripts/listRules.js');
+const listSubscriptions = require('./scripts/listSubscriptions.js');
+const listTopics = require('./scripts/listTopics.js');
+const peekDeadLetter = require('./scripts/peekDeadLetter.js');
+const peekSubscription = require('./scripts/peekSubscription.js');
+const subscriptionMsgCount = require('./scripts/subMsgCount.js');
+const watchTopic = require('./scripts/watchTopic.js');
+
+module.exports = {
+  createMessage,
+  createSubscription,
+  createTopic,
+  deleteSubscription,
+  deleteTopic,
+  listRules,
+  listSubscriptions,
+  listTopics,
+  peekDeadLetter,
+  peekSubscription,
+  subscriptionMsgCount,
+  watchTopic,
+};
