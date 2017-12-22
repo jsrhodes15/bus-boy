a862c9a9f26493a929d4c83e4a8982f21f50ef98
johnrhampton <johnrhampton@gmail.com>
2017-12-22 18:43:13 -0500

    exports functions

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const createMessage = require('./scripts/createMessage.js');
-const createSubscription = require('./scripts/createSubscription.js');
-const createTopic = require('./scripts/createTopic.js');
-const deleteSubscription = require('./scripts/deleteSubscription.js');
-const deleteTopic = require('./scripts/deleteTopic.js');
-const listRules = require('./scripts/listRules.js');
-const listSubscriptions = require('./scripts/listSubscriptions.js');
-const listTopics = require('./scripts/listTopics.js');
-const peekDeadLetter = require('./scripts/peekDeadLetter.js');
-const peekSubscription = require('./scripts/peekSubscription.js');
-const subscriptionDetails = require('./scripts/subscriptionDetails.js');
-const subscriptionMsgCount = require('./scripts/subscriptionMsgCount.js');
-const watchTopic = require('./scripts/watchTopic.js');
+const { createMessage } = require('./scripts/createMessage.js');
+const { createSubscription } = require('./scripts/createSubscription.js');
+const { createTopic } = require('./scripts/createTopic.js');
+const { deleteSubscription } = require('./scripts/deleteSubscription.js');
+const { deleteTopic } = require('./scripts/deleteTopic.js');
+const { listRules } = require('./scripts/listRules.js');
+const { listSubscriptions } = require('./scripts/listSubscriptions.js');
+const { listTopics } = require('./scripts/listTopics.js');
+const { peekDeadLetter } = require('./scripts/peekDeadLetter.js');
+const { peekSubscription } = require('./scripts/peekSubscription.js');
+const { subscriptionDetails } = require('./scripts/subscriptionDetails.js');
+const { subscriptionMsgCount } = require('./scripts/subscriptionMsgCount.js');
+const { watchTopic } = require('./scripts/watchTopic.js');
 
 module.exports = {
   createMessage,
